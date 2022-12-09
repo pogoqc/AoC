ce768fdbded0461bd2494c0ce856474f0f9050aa
David Goulet <goud2703@usherbrooke.ca>
2022-12-09 10:51:19 -0500

    day 3.2

--- 2022/day_3/part_1.ps1
+++ 2022/day_3/part_1.ps1
@@ -1,7 +1,13 @@
 $Data = Get-Content "$( $PSScriptRoot )/input.txt"
 $ResultsOrder = [Array](0..25 | Foreach-Object {$_ + [int][char]'a'}) + (0..25 | Foreach-Object {$_ + [int][char]'A'})
+
+function Find-Intersections([String]$ReferenceObject, [String]$DifferenceObject)
+{
+    Compare-Object -ReferenceObject $ReferenceObject.ToCharArray() -DifferenceObject $DifferenceObject.ToCharArray() -PassThru -IncludeEqual -ExcludeDifferent | Get-Unique
+}
+
 $Data | ForEach-Object {
     $_ -match "(\w{$($_.Length / 2)})(\w{$($_.Length / 2)})" | Out-Null
-    $Result = Compare-Object -ReferenceObject $Matches[1].ToCharArray() -DifferenceObject $Matches[2].ToCharArray() -PassThru -IncludeEqual -ExcludeDifferent | Get-Unique
+    $Result = Find-Intersections -ReferenceObject $Matches[1] -DifferenceObject $Matches[2]
     $ResultsOrder.IndexOf([int][char]$Result) + 1
 } | Measure-Object -Sum
--- 2022/day_3/part_2.ps1
+++ 2022/day_3/part_2.ps1
@@ -1,1 +1,15 @@
-$Data = Get-Content "$( $PSScriptRoot )/input.txt"
+$Data = Get-Content "$( $PSScriptRoot )/input.txt" -Raw
+$ResultsOrder = [Array](0..25 | Foreach-Object {$_ + [int][char]'a'}) + (0..25 | Foreach-Object {$_ + [int][char]'A'})
+
+function Find-Intersections([String]$ReferenceObject, [String]$DifferenceObject)
+{
+    Compare-Object -ReferenceObject $ReferenceObject.ToCharArray() -DifferenceObject $DifferenceObject.ToCharArray() -PassThru -IncludeEqual -ExcludeDifferent | Get-Unique
+}
+
+$Data -split "(.*`n*.*`n*.*`n*)" | ForEach-Object {
+    $_ -match "(.*`n*)(.*`n*)(.*`n*)" | Out-Null
+    $Result = Find-Intersections -DifferenceObject $Matches[1] -ReferenceObject $Matches[2]
+    $Result = Find-Intersections -DifferenceObject $Matches[3] -ReferenceObject $Result
+    $Result | ForEach-Object {$ResultsOrder.IndexOf([int][char]$_) + 1}
+} | Measure-Object -Sum
+
